50aac78220de603218c3226ec48b19510254de5f
Iasminv <veroneziasmin@gmail.com>
2024-09-27 14:07:55 -0300

    Added insert-data.js

--- connect.js
+++ connect.js
@@ -1,47 +1,26 @@
 require('dotenv').config()
-const { MongoClient } = require("mongodb");
 
-// Replace the following with your Atlas connection string
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB;
-const client = new MongoClient(uri);
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 async function run() {
     try {
-        // Connect to the Atlas cluster
+        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Get the database and collection on which to run the operation
-        const db = client.db("gettingStarted");
-        const col = db.collection("people");
-        // Create new documents                                                                                                                                         
-        const peopleDocuments = [
-            {
-                "name": { "first": "Alan", "last": "Turing" },
-                "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
-                "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
-                "contribs": ["Turing machine", "Turing test", "Turingery"],
-                "views": 1250000
-            },
-            {
-                "name": { "first": "Grace", "last": "Hopper" },
-                "birth": new Date(1906, 12, 9), // Dec 9, 1906                                                                                                                                 
-                "death": new Date(1992, 1, 1),  // Jan 1, 1992                                                                                                                                  
-                "contribs": ["Mark I", "UNIVAC", "COBOL"],
-                "views": 3860000
-            }
-        ]
-        // Insert the documents into the specified collection        
-        const p = await col.insertMany(peopleDocuments);
-        // Find the document
-        const filter = { "name.last": "Turing" };
-        const document = await col.findOne(filter);
-        // Print results
-        console.log("Document found:\n" + JSON.stringify(document));
-    } catch (err) {
-        console.log(err.stack);
-    }
-
-    finally {
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
 run().catch(console.dir);
+
